6f35c70151fe610622a7818115738b5b37ef8818
Oleg <daimio667@gmail.com>
2022-09-14 23:11:57 +0300

    task29

---
+++ task29/Program.cs
@@ -0,0 +1,23 @@
+﻿// Task 29 : Напишите программу, которая задаёт массив из 8 элементов 
+// и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33
+
+Console.WriteLine("Enter the number of array elements : ");
+int count = Convert.ToInt32(Console.ReadLine());
+int[] GetArray(int count)
+{
+   int[] array = new int[count]; 
+   for (int i = 0; i < array.Length; i++)
+   {
+        Console.WriteLine($"Enter array element at index {i} : ");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+   }
+   return array;
+}
+int[] resultArray = GetArray(count);
+Console.WriteLine
+($"Result array: [ {String.Join("; ", resultArray)} ]");
+int[] resultArrayA = GetArray(8);
+Console.WriteLine
+($"Result array: [ {String.Join("; ", resultArrayA)} ]");
